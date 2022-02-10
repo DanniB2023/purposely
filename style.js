16a670eb3778b7d4887d26b78d750d81dcf18b87
DanniB2023 <65875267+DanniB2023@users.noreply.github.com>
2022-02-10 17:59:16 -0500

    Add files via upload

---
+++ style.js
@@ -0,0 +1,101 @@
+// back tot top
+
+let backToTopBtn = document.querySelector('.back-to-top')
+
+window.onscroll = () => {
+    if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+        backToTopBtn.style.display = 'flex'
+    } else {
+        backToTopBtn.style.display = 'none'
+    }
+}
+
+// top nav menu
+
+let menuItems = document.getElementsByClassName('menu-item')
+
+Array.from(menuItems).forEach((item, index) => {
+    item.onclick = (e) => {
+        let currMenu = document.querySelector('.menu-item.active')
+        currMenu.classList.remove('active')
+        item.classList.add('active')
+    }
+})
+
+
+
+// on scroll animation
+
+let scroll = window.requestAnimationFrame || function(callback) {window.setTimeout(callback, 1000/60)}
+
+let elToShow = document.querySelectorAll('.play-on-scroll')
+
+isElInViewPort = (el) => {
+    let rect = el.getBoundingClientRect()
+
+    return (
+        (rect.top <= 0 && rect.bottom >= 0)
+        ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight))
+        ||
+        (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    )
+}
+
+loop = () => {
+    elToShow.forEach((item, index) => {
+        if (isElInViewPort(item)) {
+            item.classList.add('start')
+        } else {
+            item.classList.remove('start')
+        }
+    })
+
+    scroll(loop)
+}
+
+loop()
+
+// mobile nav
+
+let bottomNavItems = document.querySelectorAll('.mb-nav-item')
+
+let bottomMove = document.querySelector('.mb-move-item')
+
+bottomNavItems.forEach((item, index) => {
+    item.onclick = (e) => {
+        console.log('object')
+        let crrItem = document.querySelector('.mb-nav-item.active')
+        crrItem.classList.remove('active')
+        item.classList.add('active')
+        bottomMove.style.left = index * 25 + '%'
+    }
+})
+
+
+   
+const clientTabItems = document.querySelectorAll('.client-item');
+const showDiv = document.querySelector('.show-info');
+
+clientTabItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        showInfo(item);
+    });
+});
+
+function showInfo(item){
+    showDiv.querySelector('.show-img img').src = item.querySelector('.client-thumbnail img').src;
+    showDiv.querySelector('.show-name').innerHTML = item.querySelector('.client-name').innerHTML;
+    showDiv.querySelector('.show-designation').innerHTML = item.querySelector('.client-designation').innerHTML;
+    showDiv.querySelector('.show-description').innerHTML = item.querySelector('.client-description').innerHTML;
+    setActiveTab(item); 
+}
+
+function setActiveTab(item){
+    clientTabItems.forEach((item) => {
+        item.classList.remove('active'); // resetting active tab
+    });
+    item.classList.add('active');
+}
+
+showInfo(clientTabItems[0]); // default active tab
